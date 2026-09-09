ac079a5ca9c448259411511c6f81dec223510082
Nakao_Taketo <nkatake10@gmail.com>
2026-09-09 18:04:42 +0900

    L-chika

--- RP2354_effect_program.c
+++ RP2354_effect_program.c
@@ -4,10 +4,21 @@
 
 int main()
 {
-    stdio_init_all();
+   // LEDピンの設定
+   const uint LED_PIN = 0;
 
+   // GPIOの初期化
+   gpio_init(LED_PIN);
+   gpio_set_dir(LED_PIN, GPIO_OUT);
+
+   // 無限ループでLEDを点滅させる
     while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
+        // LEDを点灯
+        gpio_put(LED_PIN, true);
+        sleep_ms(500);
+        // LEDを消灯
+        gpio_put(LED_PIN, false);
+        sleep_ms(500);
+
     }
 }
